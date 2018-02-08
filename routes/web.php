dcf63b8535146bcb82df57f93bb40275a402339e
James Spence <Jamesaspence20@gmail.com>
2018-02-08 09:16:59 -0600

    example routes to demonstrate problem

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,36 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+use App\User;
+
+Route::middleware('web')->group(function () {
+    Route::get('login', function () {
+        $user = User::find(1);
+        if ($user === null) {
+            $user = new User();
+            $user->name = 'Test User';
+            $user->email = 'test@test.test';
+            $user->password = Hash::make('password');
+            $user->save();
+        }
+
+        Auth::login($user);
+        return redirect('/');
+    });
+
+    //Authed user works here
+    Route::get('/', function () {
+        dd(Auth::user());
+    });
+});
+
+/*
+ * Authed user doesn't work here
+ * Appears to be due to the bindings middleware
+ * happening BEFORE session is started
+ */
+Route::middleware(['bindings', 'web'])->group(function () {
+    Route::get('test', function () {
+        dd(Auth::user());
+    });
 });
