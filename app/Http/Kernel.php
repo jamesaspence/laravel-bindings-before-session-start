626801767b1f87dbe414241bd444e11d287f8e54
James Spence <Jamesaspence20@gmail.com>
2018-02-08 09:21:01 -0600

    new middleware group, webWithoutBindings

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,6 +37,15 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
+        'webWithoutBindings' => [
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            // \Illuminate\Session\Middleware\AuthenticateSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
+        ],
+
         'api' => [
             'throttle:60,1',
             'bindings',
